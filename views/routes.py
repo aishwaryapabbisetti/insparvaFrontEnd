040990288f9dbaecff068c5e3631e41908c16adf
rsunny <rsunny1993@gmail.com>
2018-12-22 19:42:11 +0100

    basic options context

--- views/routes.py
+++ views/routes.py
@@ -9,5 +9,14 @@ def index():
 
 @app.route("/test")
 def get_context():
-    context = {'message': 'Hope it works'}
-    return render_template('select2.html', title='Home', context=context)
+    options = []
+    for i in range(0, 20000):
+        temp = str(i + 1)
+        while len(temp) < 5:
+            temp = '0' + temp
+        options.append(temp)
+
+    data = {
+        'options': options
+    }
+    return render_template('select2.html', title='Home', data=data)
